f2d2ee6e0aa9b059018629e9c34a44515ae8a533
micnil <michaelnilsson90@hotmail.com>
2015-02-01 14:19:46 +0800

    fixed stupid error

--- geolocation.js
+++ geolocation.js
@@ -1,22 +1,22 @@
 
 var Geolocation = {
-  sendRequestFunction: null,
+  
   options: {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0
   },
-  success: function(pos) {
-    if (sendRequestFunction) {
-      sendRequestFunction(pos.coords.latitude, pos.coords.longitude);
-    };
-  },
+
   error: function(err) {
     console.warn('ERROR(' + err.code + '): ' + err.message);
   },
   updatePosition: function(callbackFunction) {
     sendRequestFunction = callbackFunction;
-    navigator.geolocation.getCurrentPosition(Geolocation.success, Geolocation.error, Geolocation.options);
+     navigator.geolocation.getCurrentPosition(function(pos) {
+            if (typeof callbackFunction == "function") {
+                callbackFunction(pos.coords.latitude, pos.coords.longitude);
+            }
+        }, Geolocation.error, Geolocation.options);
   },
 };
 
